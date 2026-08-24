ce360f36d6d3cbf3ecf6f1fd43ee6b4b6d0fd404
Sushmita Gollena <sgollena@qti.qualcomm.com>
2026-08-23 23:12:34 -0700

    kgsl: Prevent bind to VBO entries pending free in deferred worker
    Add a pending_free check in kgsl_sharedmem_bind_worker to skip binding
    operations for VBO entries that are being freed. This prevents refcount
    leaks and use-after-free by ensuring no new references are taken on
    entries pending destruction.
    
    Change-Id: I51af0cde8d3c63db50a3788ba9e6d88c453169a6
    Signed-off-by: Shiv Kumar <shikum@qti.qualcomm.com>
    Signed-off-by: Sushmita Gollena <sgollena@qti.qualcomm.com>
    Signed-off-by: Sai Krishna Kota <saikota@qti.qualcomm.com>

--- drivers/gpu/msm/kgsl_vbo.c
+++ drivers/gpu/msm/kgsl_vbo.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/file.h>
@@ -508,18 +508,24 @@ static void kgsl_sharedmem_bind_worker(struct work_struct *work)
 	int i;
 
 	for (i = 0; i < op->nr_ops; i++) {
-		if (op->ops[i].op == KGSL_GPUMEM_RANGE_OP_BIND)
+		if (op->ops[i].op == KGSL_GPUMEM_RANGE_OP_BIND) {
+			spin_lock(&op->ops[i].entry->priv->mem_lock);
+			if (op->ops[i].entry->pending_free) {
+				spin_unlock(&op->ops[i].entry->priv->mem_lock);
+				continue;
+			}
+			spin_unlock(&op->ops[i].entry->priv->mem_lock);
 			kgsl_memdesc_add_range(op->target,
 				op->ops[i].start,
 				op->ops[i].last,
 				op->ops[i].entry,
 				op->ops[i].child_offset);
-		else
+		} else {
 			kgsl_memdesc_remove_range(op->target,
 				op->ops[i].start,
 				op->ops[i].last,
 				op->ops[i].entry);
-
+		}
 	}
 
 	/* Wake up any threads waiting for the bind operation */
